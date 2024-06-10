097cbf9859ff7f8ffc8cecb3700a067dcfe96b3d
sipofnightshade <gregorymcmillan96@gmail.com>
2024-06-10 11:26:42 -0400

    fixed main layout overflow  and added breakpoints component

---
+++ src/lib/components/TailwindBreakpoints.svelte
@@ -0,0 +1,10 @@
+<div
+	class="absolute bottom-2 right-2 scroll-py-14 rounded-lg bg-violet-500 p-2 text-sm font-bold uppercase text-black"
+>
+	<span class="sm:hidden">base</span>
+	<span class="hidden sm:block md:hidden">sm</span>
+	<span class="hidden md:block lg:hidden">md</span>
+	<span class="hidden lg:block xl:hidden">lg</span>
+	<span class="hidden xl:block 2xl:hidden">xl</span>
+	<span class="3xl:hidden hidden 2xl:block">xl</span>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,20 @@
 <script lang="ts">
 	import '../app.css';
 	import type { LayoutData } from './$types';
+	import { dev } from '$app/environment';
+	import TailwindBreakpoints from '$lib/components/TailwindBreakpoints.svelte';
 
 	export let data: LayoutData;
 </script>
 
 <div
-	class="absolute text-slate-200 top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"
+	class="absolute top-0 z-[-2] h-screen w-screen overflow-hidden bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] text-slate-200"
 >
-	<slot></slot>
+	<div class="h-full w-full overflow-y-auto">
+		<slot />
+	</div>
 </div>
+
+{#if dev}
+	<TailwindBreakpoints />
+{/if}
