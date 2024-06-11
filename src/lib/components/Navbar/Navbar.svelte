36ad03825d2cafe9787bd4da5bd97e5eaf972bb1
sipofnightshade <gregorymcmillan96@gmail.com>
2024-06-11 11:02:08 -0400

    added navbar and footer components

---
+++ src/lib/components/Navbar/Navbar.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import Logo from '$lib/icons/Logo.svelte';
+</script>
+
+<div class="w-full border-b border-zinc-800">
+	<div class="container mx-auto flex h-16 w-full items-center justify-between px-2">
+		<Logo class="w-24" />
+
+		<p class="font-bold">PATCH 1.1</p>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 	import type { LayoutData } from './$types';
 	import { dev } from '$app/environment';
 	import TailwindBreakpoints from '$lib/components/TailwindBreakpoints.svelte';
+	import Navbar from '$lib/components/Navbar/Navbar.svelte';
+	import Footer from '$lib/components/Footer/Footer.svelte';
 
 	export let data: LayoutData;
 </script>
@@ -10,8 +12,12 @@
 <div
 	class="absolute top-0 z-[-2] h-screen w-screen overflow-hidden bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] text-slate-200"
 >
-	<div class="h-full w-full overflow-y-auto">
-		<slot />
+	<div class="flex h-full w-full flex-col justify-between gap-y-4 overflow-y-auto">
+		<Navbar />
+		<div class="container mx-auto px-2 py-8">
+			<slot />
+		</div>
+		<Footer />
 	</div>
 </div>
 
