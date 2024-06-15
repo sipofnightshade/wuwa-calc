cfbc8145c3bcff833489cebf713ca5c7311da74c
sipofnightshade <gregorymcmillan96@gmail.com>
2024-06-15 10:54:49 -0400

    added route placeholders

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const routes = ['characters', 'weapons', 'echoes', 'simulator'];
+</script>
+
+<div class="mx-auto w-fit space-y-4">
+	<h1 class="text-4xl font-bold">Welcome, Rovers!</h1>
+
+	<div class="flex justify-center gap-4">
+		{#each routes as route}
+			<a href="/{route}" class="flex flex-col items-center gap-y-2">
+				<div class="h-28 w-28 rounded-sm bg-zinc-800"></div>
+				<p class="font-bold capitalize">{route}</p>
+			</a>
+		{/each}
+	</div>
+</div>
+
+<!-- USE A SNIPPET HERE TO HIGHLIGHT THE SIMULATOR DIFFERENTLY -->
---
+++ src/routes/simulator/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<div class=" bg-red-500"></div>
