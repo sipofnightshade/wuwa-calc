7d4283cb562a98370675360a0f48d886cd84dfe1
sipofnightshade <gregorymcmillan96@gmail.com>
2024-06-21 18:47:40 -0400

    added-- item group component and test images

---
+++ src/lib/components/common/RequiredItemGroup.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  type Props = { url: string; count: string };
+
+  let { url, count }: Props = $props();
+</script>
+
+<div class="flex items-end gap-0.5">
+  <img class="h-10 w-10" src={url} alt="item" />
+  <span class="text-lg font-bold">x{count}</span>
+</div>
