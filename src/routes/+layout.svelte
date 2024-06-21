b29fd41489421f3715e5b04c48e15f87b6c98cf4
sipofnightshade <gregorymcmillan96@gmail.com>
2024-06-20 23:10:24 -0400

    updated-- main layout to justify-between only on the home page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,16 @@
   import TailwindBreakpoints from '$lib/components/feature/TailwindBreakpoints.svelte';
   import Navbar from '$lib/components/layout/Navbar.svelte';
   import Footer from '$lib/components/layout/Footer.svelte';
+  import { page } from '$app/stores';
 </script>
 
 <div
   class="absolute top-0 z-[-2] h-screen w-screen overflow-hidden bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(119,132,198,0.3),rgba(255,255,255,0))] text-slate-200"
 >
-  <div class="flex h-full w-full flex-col justify-between gap-y-4 overflow-y-auto">
+  <div
+    class="flex h-full w-full flex-col gap-y-4 overflow-y-auto"
+    class:justify-between={$page.url.pathname === '/'}
+  >
     <Navbar />
     <div class="container mx-auto px-2 py-8">
       <slot />
