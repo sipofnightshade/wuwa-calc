20bacfb89bd370a8e88be3a3bc8074493435a337
sipofnightshade <gregorymcmillan96@gmail.com>
2024-06-21 13:33:45 -0400

    added-- stargroup and separator components

---
+++ src/lib/components/common/Separator.svelte
@@ -0,0 +1,1 @@
+<div class="h-3/5 w-px bg-zinc-500"></div>
---
+++ src/lib/components/common/StarGroup.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  type Ratings = 1 | 2 | 3 | 4 | 5;
+  type Props = { rating: Ratings };
+
+  let { rating }: Props = $props();
+
+  const ratingMap = {
+    1: { img: 'One_Star', list: [1] },
+    2: { img: 'Two_Star', list: [1, 2] },
+    3: { img: 'Three_Star', list: [1, 2, 3] },
+    4: { img: 'Four_Star', list: [1, 2, 3, 4] },
+    5: { img: 'Five_Star', list: [1, 2, 3, 4, 5] }
+  };
+</script>
+
+<div class="flex">
+  {#each ratingMap[rating].list as star (star)}
+    <img class="h-5" src="/rating/{ratingMap[rating].img}.svg" alt="rating" />
+  {/each}
+</div>
