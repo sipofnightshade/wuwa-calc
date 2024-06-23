0d02e1ab1d4c74d5a9fc813d6500aab2fecdd155
sipofnightshade <gregorymcmillan96@gmail.com>
2024-06-23 11:33:56 -0400

    added-- global types

---
+++ src/lib/types/globals.ts
@@ -0,0 +1,13 @@
+export type WW_Elements = 'aero' | 'electro' | 'glacio' | 'fusion' | 'havoc' | 'spectro';
+export type WW_Weapons = 'sword' | 'pistols' | 'broadsword' | 'rectifier' | 'gauntlets';
+export type WW_Forte_labels =
+  | 'Normal Attack'
+  | 'Basic Attack'
+  | 'Heavy Attack'
+  | 'Mid-air Attack'
+  | 'Resonance Skill'
+  | 'Forte Circuit'
+  | 'Inherent Skill'
+  | 'Resonance Liberation'
+  | 'Intro Skill'
+  | 'Outro Skill';
