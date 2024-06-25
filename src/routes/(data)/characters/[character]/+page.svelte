6dd9be98e8c594f2c8365700353a5a63822bc212
sipofnightshade <gregorymcmillan96@gmail.com>
2024-06-25 02:47:26 -0400

    added-- toggle switch to [character] with inherent skills and wavebands

--- src/routes/(data)/characters/[character]/+page.svelte
+++ src/routes/(data)/characters/[character]/+page.svelte
@@ -6,18 +6,27 @@
   import RequiredItemGroup from '$lib/components/common/RequiredItemGroup.svelte';
   import Separator from '$lib/components/common/Separator.svelte';
   import StarGroup from '$lib/components/common/StarGroup.svelte';
+  import ToggleSwitch from '$lib/components/common/ToggleSwitch.svelte';
   import DamageFormatSelector from '$lib/components/feature/DataTable/DamageFormatSelector.svelte';
   import Table from '$lib/components/feature/DataTable/Table.svelte';
   import type { ValuesFormat } from '$lib/components/feature/DataTable/types';
   import StatEditorPanel from '$lib/components/feature/StatEditorPanel/StatEditorPanel.svelte';
+  import { colors } from '$lib/data/colors';
   // import type { PageData } from './$types';
   // let{ data }: PageData = $props();
+
+  const element = 'electro';
 
   let skillLevels: Record<string, number> = $state({
     normal: 10,
     skill: 10,
     circuit: 10,
     liberation: 10
+  });
+
+  let activatedInherentSkills: Record<number, boolean> = $state({
+    0: false,
+    1: false
   });
 
   let activatedWavebands: Record<number, boolean> = $state({
@@ -48,6 +57,19 @@
     {
       id: 'liberation',
       name: 'Resonance Liberation',
+      src: '/temp/T_IconDevice_JiyanM6_UI.png'
+    }
+  ];
+
+  const inherentSkills = [
+    {
+      id: 1,
+      name: 'Normal Attack',
+      src: '/temp/T_IconDevice_JiyanM6_UI.png'
+    },
+    {
+      id: 2,
+      name: 'Resonance Skill',
       src: '/temp/T_IconDevice_JiyanM6_UI.png'
     }
   ];
@@ -116,7 +138,7 @@
       <Separator />
       <p>BROADBLADE</p>
       <Separator />
-      <p class="text-electro">ELECTRO</p>
+      <p class={colors[element].text}>ELECTRO</p>
     </div>
 
     <!-- character item requirements -->
@@ -132,36 +154,43 @@
   <StatEditorPanel />
 </div>
 
+{#snippet SectionTitle(title, shortDesc)}
+  <h2 class="flex gap-4 text-2xl font-bold">
+    <span>{title}</span>
+    <span class="text-zinc-500">/</span>
+    <span class="text-zinc-500">{shortDesc}</span>
+  </h2>
+{/snippet}
+
+{#snippet SkillHeader(skill)}
+  <div class="space-y-4">
+    <ImageHeader title={skill.name} subtitle="Skill name goes here" src={skill.src} />
+    <ContentBlock {element} />
+  </div>
+{/snippet}
+
+<!-- skills and damage tables -->
 <section class="mt-16 flex flex-col gap-y-6">
-  <!-- title -->
   <div class="flex items-end justify-between">
-    <h2 class="flex gap-4 text-2xl font-bold">
-      <span>Forte</span>
-      <span class="text-zinc-500">/</span>
-      <span class="text-zinc-500">Resonator Abilities</span>
-    </h2>
+    {@render SectionTitle('Forte', 'Resonator Abilities')}
     <div class="flex items-center gap-4 font-bold uppercase">
       <div class="flex items-center gap-1">
         <img class="h-6 w-6" src="/stats/T_Iconpropertyredcrit_UI.png" alt="stat" />
         <p>Crit DMG</p>
-        <p class="text-electro">+12%</p>
+        <p class={colors[element].text}>+12%</p>
       </div>
       <Separator height="h-5" />
       <div class="flex items-center gap-1">
         <img class="h-6 w-6" src="/stats/T_Iconpropertyredattack_UI.png" alt="stat" />
         <p>ATK%</p>
-        <p class="text-electro">+12%</p>
+        <p class={colors[element].text}>+12%</p>
       </div>
     </div>
   </div>
 
-  <!-- skills and damage tables -->
   {#each skills as skill, i}
     <div class="grid grid-cols-2 gap-12 border-t border-zinc-700 pt-6">
-      <div class="space-y-4">
-        <ImageHeader title={skill.name} subtitle="Skill name goes here" src={skill.src} />
-        <ContentBlock element="electro" />
-      </div>
+      {@render SkillHeader(skill)}
 
       <div class="space-y-2">
         <RangeSelector
@@ -172,6 +201,44 @@
         />
         <DamageFormatSelector bind:format />
         <Table data="..." {format} />
+      </div>
+    </div>
+  {/each}
+</section>
+
+<!-- inherent skills -->
+<section class="mt-16 flex flex-col gap-y-6">
+  {@render SectionTitle('Inherent Skills', 'Circuit Upgrades')}
+
+  {#each inherentSkills as skill}
+    <div class="grid grid-cols-[2fr,1fr] gap-12 border-t border-zinc-700 pt-6">
+      {@render SkillHeader(skill)}
+
+      <div class="flex justify-end">
+        <ToggleSwitch
+          label="inherentSkill_{skill.id}"
+          {element}
+          bind:value={activatedInherentSkills[skill.id]}
+        />
+      </div>
+    </div>
+  {/each}
+</section>
+
+<!-- resonance chain -->
+<section class="mt-16 flex flex-col gap-y-6">
+  {@render SectionTitle('Resonance Chain', 'Wavebands')}
+
+  {#each wavebands as waveband}
+    <div class="grid grid-cols-[2fr,1fr] gap-12 border-t border-zinc-700 pt-6">
+      {@render SkillHeader(waveband)}
+
+      <div class="flex justify-end">
+        <ToggleSwitch
+          label="inherentSkill_{waveband.id}"
+          {element}
+          bind:value={activatedWavebands[waveband.id]}
+        />
       </div>
     </div>
   {/each}
